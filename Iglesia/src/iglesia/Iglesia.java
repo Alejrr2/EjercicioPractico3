910667ed0d0c964f98e280336cf00c85938b5de4
Alejrr2 <124023428+Alejrr2@users.noreply.github.com>
2023-07-19 23:31:52 -0600

    Add files via upload

---
+++ Iglesia/src/iglesia/Iglesia.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package iglesia;
+
+/**
+ *
+ * @author alejh
+ */
+public class Iglesia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // En en esta parte del codigo llamada main solo
+        // llamaremos la funcion que ya creamos en la clase calculador
+        Calculador calculador = new Calculador();
+        calculador.iglesiaDatos();
+    }
+}
---
+++ Iglesia/src/iglesia/infoIglesia.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package iglesia;
+
+/**
+ *
+ * @author alejh
+ */
+public class infoIglesia {
+    // En esta zona se definen los valores que vamos a usar
+    public double totalEarnings = 0;
+    public String feligresName = "";
+    public String feligresID = "";
+    public int feligresDiez = 0;
+    // Aqui creamos un constructor que sera utilizado mas adelante
+    public infoIglesia(String feligresName, String feligresID, int feligresDiez, double totalEarnings) {
+        this.feligresDiez = feligresDiez;
+        this.feligresID = feligresID;
+        this.feligresName = feligresName;
+        this.totalEarnings = totalEarnings;
+    }
+    // zona de gatters
+    public double getTotalEarnings() {
+        return totalEarnings;
+    }
+
+    public String getFeligresName() {
+        return feligresName;
+    }
+
+    public String getFeligresID() {
+        return feligresID;
+    }
+
+    public int getFeligresDiez() {
+        return feligresDiez;
+    }
+}
+
+    
+
